354d2aeab8b44cb96126d3aa23a4cf244da0edde
Abd Frehat <abdmond6@gmail.com>
2026-03-31 02:28:54 +0300

    tryi

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://api.mabasol.com/api/v1',
     // Base URL for static assets (/uploads/...) in production
-    imageBaseUrl: 'https://mabasol.com'
+    imageBaseUrl: 'https://api.mabasol.com'
 };
